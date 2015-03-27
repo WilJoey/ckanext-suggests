3d76b73ea915364465f27387b59ee53c1d3ff4d9
WilJoey Lin <wiljoey@gmail.com>
2015-03-27 17:41:17 +0800

    ext paster init

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+    name='ckanext-suggests',
+    version=version,
+    description="dataset suggests",
+    long_description='''
+    ''',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='WilJoey',
+    author_email='wiljoey@gmail.com',
+    url='',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.suggests'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points='''
+        [ckan.plugins]
+        # Add plugins here, e.g.
+        # myplugin=ckanext.suggests.plugin:PluginClass
+    ''',
+)
